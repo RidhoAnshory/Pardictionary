bc36ea2d5ec7ec70ee8bef3416c6029061f7fc50
RidhoAnshory <muhridhoanshory@gmail.com>
2020-10-24 22:16:38 +0800

    add vocab model I

---
+++ lib/models/vocab_forest_model.dart
@@ -0,0 +1,52 @@
+class VocabForest {
+  String imgUrl;
+  String eng;
+  String engPron;
+  String ind;
+  String meaning;
+  String example;
+
+  VocabForest({
+    this.imgUrl,
+    this.eng,
+    this.engPron,
+    this.ind,
+    this.meaning,
+    this.example,
+  });
+}
+
+final List<VocabForest> vocabsforest = [
+  VocabForest(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabForest(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabForest(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabForest(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+];
---
+++ lib/models/vocab_hotel_model.dart
@@ -0,0 +1,52 @@
+class VocabHotel {
+  String imgUrl;
+  String eng;
+  String engPron;
+  String ind;
+  String meaning;
+  String example;
+
+  VocabHotel({
+    this.imgUrl,
+    this.eng,
+    this.engPron,
+    this.ind,
+    this.meaning,
+    this.example,
+  });
+}
+
+final List<VocabHotel> vocabsforest = [
+  VocabHotel(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabHotel(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabHotel(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+  VocabHotel(
+      imgUrl: 'assets/images/forest.jpg',
+      eng: 'For·est',
+      engPron: 'ˈfôrəst',
+      ind: 'Hutan',
+      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      example:
+          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+];
