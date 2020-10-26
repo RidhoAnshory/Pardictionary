8f7fa85f7a29c60be7db5787d41bd9de2cbae7d3
Muhammad Ridho Anshory <muhridhoanshory@gmail.com>
2020-10-26 14:47:23 +0800

    Update vocab_hotel_model.dart

--- lib/models/vocab_hotel_model.dart
+++ lib/models/vocab_hotel_model.dart
@@ -19,34 +19,37 @@ class VocabHotel {
 final List<VocabHotel> vocabshotel = [
   VocabHotel(
       imgUrl: 'assets/images/vocabs/hotel/concierge.jpg',
-      eng: 'For·est',
-      engPron: 'ˈfôrəst',
-      ind: 'Hutan',
-      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      eng: 'Concierge',
+      engPron: 'kɑːn.siˈerʒ',
+      ind: 'Penjaga Pintu',
+      meaning:
+          'A person working in a hotel, etc. whose job is to help guests, for example by giving them information about restaurants, local places to see, etc.',
       example:
-          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+          'Our company offers concierge services for both individual and corporate customers.'),
   VocabHotel(
       imgUrl: 'assets/images/vocabs/hotel/lobby.jpg',
-      eng: 'For·est',
-      engPron: 'ˈfôrəst',
-      ind: 'Hutan',
-      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      eng: 'Lobby',
+      engPron: 'ˈlɑː.bi',
+      ind: 'Lobi',
+      meaning:
+          'The (large) room into which the main entrance door opens in a hotel or other large building.',
       example:
-          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+          'As you enter the lobby, you’ll see the elevators on your right.'),
   VocabHotel(
       imgUrl: 'assets/images/vocabs/hotel/receptionist.jpg',
-      eng: 'For·est',
-      engPron: 'ˈfôrəst',
-      ind: 'Hutan',
-      meaning: 'A large area covered chiefly with trees and undergrowth.',
+      eng: 'Receptionist',
+      engPron: 'rɪˈsep.ʃən.ɪst',
+      ind: 'Resepsionis',
+      meaning:
+          'A person who works in a place such as a hotel, office, or hospital, who welcomes and helps visitors and answers the phone.',
       example:
-          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+          'Participants included 46 general practitioners, 9 practice managers and 1 receptionist.'),
   VocabHotel(
       imgUrl: 'assets/images/vocabs/hotel/security.jpg',
-      eng: 'For·est',
-      engPron: 'ˈfôrəst',
-      ind: 'Hutan',
-      meaning: 'A large area covered chiefly with trees and undergrowth.',
-      example:
-          'Tropical rain forests used to cover 10% of the earth`s surface.'),
+      eng: 'Security',
+      engPron: 'səˈkjʊr.ə.t̬i',
+      ind: 'Petugas Keamanan',
+      meaning:
+          'The group of people or a person who responsible for keeping buildings or other areas safe.',
+      example: 'If anyone gives you trouble, just call security.'),
 ];
